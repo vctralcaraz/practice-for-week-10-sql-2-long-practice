5b596e7b7ed1acc8b3586c2eb8a5fba177bb0d47
Victor Alcaraz <vctralcaraz@gmail.com>
2024-03-26 15:24:21 -0700

    phase 3 complete

--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,2 +1,7 @@
 -- Find All the Toys for Hermione's cats
 -- Your code here
+SELECT toys.name FROM toys
+JOIN cats ON toys.cat_id = cats.id
+JOIN cat_owners ON cats.id = cat_owners.cat_id
+JOIN owners ON owners.id = cat_owners.owner_id
+WHERE owners.first_name = 'Hermione';
